2ae18049625c51988a8d4b76da298b6d0a307075
Youssef BESTAVROS <ybesta@teaching.CS.McGill.CA>
2020-04-29 19:11:12 -0400

    Finished and commented the first question

--- Q1/divisor.sh
+++ Q1/divisor.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 div () {
-	#Checks whether the correct number of arguments is passed in the div() function
-	if [ $# -ne 2 ]
-	then
-		echo "Incorrect number of arguments given to $0"
-		exit 1
-	elif [ $1 -eq 0 ] && [ $2 -eq 0 ] 
+	#div function that outputs the largest divisor between two given integers
+	#This assumes that the user correctly inputs TWO INTEGERS 
+	
+	#If both arguments passed are 0 
+	if [ $1 -eq 0 ] && [ $2 -eq 0 ] 
 	then 
-		echo "Cannot have the largest divisor between two zeros"
+		echo "any number (Cannot have the largest divisor between two zeros)"
 		exit 1
 	fi
 	
-	#Setting the two inputs of this function (to be always positive
+	#Setting the two inputs of this function (to be always positive), using bc to 
+	#evaluate the sqrt(x^2).
 	X=`echo "sqrt($1*$1)" | bc`
 	Y=`echo "sqrt($2*$2)" | bc`
 	N=0	
-	#Takes care of the case if one of the arguments is zero
+
+	#If conditions below take care of the case if one of the arguments is zero;
+	#We just return the other number.
 	if [ $X -eq 0 ]
 	then
-		return $Y
+		echo $Y
+		exit 0
 	elif [ $Y -eq 0 ]
 	then 
-		return $X
+		echo $X
+		exit 0
 	fi
 
-	#Picks the value of N depending on whether X or Y is smaller 
+	#Picks the value of N depending on whether X or Y is smaller. 
 	if [ $X -le $Y ]
 	then
 		N=$X
@@ -34,33 +38,34 @@ div () {
 		N=$Y
 	fi
 	
-
-	while [ `echo "$X%$N" | bc` -ne 0 ] && [ `echo "$Y%$N" | bc` -ne 0 ]
+	#Checks whether both numbers are divisible by N. If one of them is not, then 
+	#decrease N by 1, else if both are divisible, then stop the while loop.
+	while [ `echo "$X%$N" | bc` -ne 0 ] || [ `echo "$Y%$N" | bc` -ne 0 ]
 	do 
 		N=`echo "$N-1" | bc`
-		echo "$N"
 	done
 
-	return $N
+	#Returns the value of N 
+	echo $N
 }
 
+#While loop condition keeps going if answer of the user is y
 answer="y"
 
 while [ $answer = "y" ]
 do
-
+	#Asking user for numbers
 	echo "Enter first number:"
-	read X
+	read X 	#Reads the user input
 	echo "Enter second number:"
-	read Y
+	read Y  #Reads the user input
 
+	#Calls in div and assigns it to N 
 	N=$( div $X $Y )
-
+	
+	#Ouputs the answer
 	echo "Largest divisor of $X and $Y is $N"
+	#Asks the user if they want to continue and updates answer 
 	echo "Do you wish to continue ?"
-	read $answer
+	read answer
 done
-
-
-
-
