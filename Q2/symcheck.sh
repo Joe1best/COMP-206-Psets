530212c36981729cfbb8c2b619037d8c092cadf7
Youssef BESTAVROS <ybesta@teaching.CS.McGill.CA>
2020-04-30 00:23:54 -0400

    Finished the symcheck.sh along with a test.txt that was created

--- Q2/symcheck.sh
+++ Q2/symcheck.sh
@@ -12,16 +12,28 @@ filename=$1
 #current directory
 directory=$PWD
 
-#Runs the ls command along with grep to filter out the files in the 
-#ucrrent directory with the filename as the filter.
-files=`ls $directory | grep $filename`
 
-#If no matches, closes the script and informs the user 
-if [ $? -eq 1 ]
+#Checks whether filename variable exists in the current directory 
+if [ ! -f "$filename" ]
 then 
 	echo "Unable to locate the file $filename in directory $directory"
 	exit 2;
 fi
 
-
-
+#while that reads the $filename line by line and executes the c file. THIS ASSUMES
+#THAT THE OUT FILE IS CALLED symword AND IS IN THE SAME DIRECTORY AS THIS BASH SCRIPT
+`gcc -o symword symword.c` #compiles the C file 
+while read line
+do 
+	`./symword $line`
+	c=`echo $?`#saves the value of the exit code
+       	#If exit code is equal to 1, then the word is not symmetric as defined 
+	#in the symword.c file. If it is equal to 0, then is symmetric	
+	if [ $c -eq 1 ]
+	then 
+		echo "$line is not symmetric"
+	elif [ $c -eq 0 ]
+	then 
+		echo "$line is symmetric"
+	fi
+done < $filename
