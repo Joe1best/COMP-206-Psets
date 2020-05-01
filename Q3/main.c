8f54990ddb1543dbdf442985f2d07834d7991aa9
Youssef BESTAVROS <ybesta@teaching.CS.McGill.CA>
2020-05-01 13:27:33 -0400

    Did some fixing

--- Q3/main.c
+++ Q3/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
 	//Checks that the number of arguments given to the command line is correct. Else it prints the error message
 	//below, along with an exit code of 1
 	if (argc!=4){
-		printf("You need to give this function arguments of type [FILENAME/PATH TO FILENAME] [WORD1] [WORD2]");
+		printf("You need to give this function arguments of type [FILENAME/PATH TO FILENAME] [WORD1] [WORD2]\n");
 		exit(1);
 	}	
 	//File pointer
@@ -22,8 +22,9 @@ int main(int argc, char *argv[]){
 
 	//Checks whether the file exists in the path given using the fopen() function, this method will attempt to 
 	//open the file and will return false if it could not. 
-	if (!(fPointer=fopen(filename,"r"))|| tempFile = NULL){
-		printf("Error, could not find file in location %s",filename);
+	if (!(fPointer=fopen(filename,"r"))|| tempFile == NULL){
+		printf("Error, could not find file in location %s\n",filename);
+		exit(1);
 	}
 
 	//Opens the input file
@@ -33,24 +34,47 @@ int main(int argc, char *argv[]){
 	//Defining the three word variables
 	char *word1 = argv[2];
 	char *word2 = argv[3];
-	char word[1000];
 
-	//The result of the replace method
-	//
+	//The result of the replace method	
 	char *result;
-	//To read each word in the file, I use the fscanf function, This function will read the file fPointer based 
-	//on the type of argument given (string, hence %s) and how long this argument is expected to be (in our case, 
-	//max 1000 word length) and it saves the word in the "word" variable
-	while (fscanf(fPointer, "%1000s",word) ==1) {
-		result = replace(word, word1, word2);
-		fputs(result,tempFile);
+	//To read each word in the file, I read every character in the file using fgetc(). Until I detect that the 
+	//character is not an alphabet, I concatenate this character to the variable "word" forming a word in the
+	//file. Once the word is done (detect a space or any other character than an alphabet), I use the replace
+	//function and write that in the new file. 
+	char word[1000]="";
+	char c;
+	while ((c=fgetc(fPointer)) !=EOF ) {
+		//If the character is an alphabet (capital or lower case) (I check it using the ASCII value)
+		if ((c>=65 && c<=90) || (c>=97 && c<=122)){	
+			//Concatenate the character to the string word 
+			strncat(word,&c,1);
+		//If it is not an alphabet (a space, a tab, an enter, or any other symbols).
+		} else {
+			//Take care of the condition if the beginning of the file just starts with 
+			//a weird character or the fact that there could be multiple spaces 
+			if (strlen(word) >= 1){
+				//Uses the replace function in replace.c
+				result = replace(word,word1,word2);
+			}
 
+			//Puts the result in the file
+			fputs(result,tempFile);
+
+			//Reset the word in order to start filling up the next one in the array
+			word[0] = '\0';
+			
+			//Puts the character that is not an alphabet in the file 
+			fputc(c,tempFile);
+		}
 	}
-
+	
+	//Closes both files
 	fclose(fPointer);
 	fclose(tempFile);
 
+	//Remove the old file
 	remove(filename);
-
-	rename("temp.txt", path);
+	
+	//Rename the temp one to the old filename
+	rename("temp.txt", filename);
 }
