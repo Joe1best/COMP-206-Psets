c7266a50854175e329169a6d4c9a659c7cfd56be
Youssef BESTAVROS <ybesta@teaching.CS.McGill.CA>
2020-05-02 00:19:57 -0400

    fixed some issues with symcheck

--- Q2/symcheck.sh
+++ Q2/symcheck.sh
@@ -4,7 +4,7 @@
 if [ $# -ne 1 ]
 then 
 	echo "This script expects only one argument of type .txt"
-	exit 1;
+	exit 1
 fi
 
 #filename argument
@@ -17,23 +17,27 @@ directory=$PWD
 if [ ! -f "$filename" ]
 then 
 	echo "Unable to locate the file $filename in directory $directory"
-	exit 2;
+	exit 2
 fi
-
 #while that reads the $filename line by line and executes the c file. THIS ASSUMES
 #THAT THE OUT FILE IS CALLED symword AND IS IN THE SAME DIRECTORY AS THIS BASH SCRIPT
-`gcc -o symword symword.c` #compiles the C file 
+
+#Compiles the C file
+`gcc -o symword symword.c` 
+
 while read line
 do 
+	echo "$line"
 	`./symword $line`
-	c=`echo $?`#saves the value of the exit code
-       	#If exit code is equal to 1, then the word is not symmetric as defined 
+	#saves the value of the exit code
+	c=`echo $?`
+	#If exit code is equal to 1, then the word is not symmetric as defined 
 	#in the symword.c file. If it is equal to 0, then is symmetric	
 	if [ $c -eq 1 ]
 	then 
 		echo "$line is not symmetric"
 	elif [ $c -eq 0 ]
 	then 
-		echo "$line is symmetric"
+		echo  "$line is symmetric"
 	fi
 done < $filename
